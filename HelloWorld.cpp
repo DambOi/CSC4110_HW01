ee28a94c6ce964cb47dc0632161bb512ed77cb94
James Bradbury <fe4034@wayne.edu>
2020-09-15 21:42:47 -0400

    adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: James Bradbury <fe4034@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
